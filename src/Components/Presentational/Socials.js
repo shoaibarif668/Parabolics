fc549675fa7d8ee27161f2273a15dbf48c79dd3f
Shoaib Arif <shoaibarif@crystal-lite.net>
2022-01-28 21:02:59 +0500

    social links update

--- src/Components/Presentational/Socials.js
+++ src/Components/Presentational/Socials.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import styles from "../Styles/Social.module.css"
-import {FaDiscord,FaTelegram} from "react-icons/fa"
+import {FaDiscord, FaTelegram} from "react-icons/fa"
+
 const Socials = () => {
     return (
         <>
-        <section className={styles.social__wrapper}>
-            <h1>Join us on <span>Discord & Telegram</span></h1>
-            <div className={styles.social__links}>
-                <a href="https://discord.com/" target="_blank" rel="noopener noreferrer" className={styles.discord}><FaDiscord/></a>
-                <a href="https://telegram.org/" target="_blank" rel="noopener noreferrer" className={styles.telegram}><FaTelegram/></a>
-            </div>
-        </section>
+            <section className={styles.social__wrapper}>
+                <h1>Join us on <span>Discord & Telegram</span></h1>
+                <div className={styles.social__links}>
+                    <a href="https://discord.gg/UkHeZYKWH9" target="_blank" rel="noopener noreferrer"
+                       className={styles.discord}><FaDiscord/></a>
+                    <a href="https://t.me/parabolicsllc" target="_blank" rel="noopener noreferrer"
+                       className={styles.telegram}><FaTelegram/></a>
+                </div>
+            </section>
         </>
     )
 }
